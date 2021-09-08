317a29bc2fc8115102bdd4fe153331c3594cc910
phamhtthu <phamhtthu@gmail.com>
2021-09-08 15:18:16 +0700

    Add common use and helper function

---
+++ src/common/dto/pagination-query.dto.ts
@@ -0,0 +1,10 @@
+import { IsOptional } from 'class-validator';
+export class PaginationQueryDto {
+  
+  @IsOptional()
+  limit: number;
+
+  @IsOptional()
+  offset: number;
+  
+}
---
+++ src/common/enum/filter.enum.ts
@@ -0,0 +1,8 @@
+export enum Status {
+    'Active' = 1,
+    'InActive' = 0,
+}
+export enum SortQuery {
+    'Asc' = 1,
+    'Desc' = -1
+}
