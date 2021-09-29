127ba65900ba4faf449feee80a58ce9c1f6d6f7c
phamhtthu <phamhtthu@gmail.com>
2021-09-29 17:42:41 +0700

    ada

--- src/workcenter/workcenter.module.ts
+++ src/workcenter/workcenter.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common"
-import { LaborDatabaseModule } from "src/model/labor-database.module"
-import { ResourceDatabaseModule } from "src/model/resource-database.module"
 import { UserDatabaseModule } from "src/model/user-database.module"
+import { ResourceDatabaseModule } from "src/model/resource-database.module"
 import { WorkCenterDatabaseModule } from "src/model/workcenter-database.module"
 import { SharedModule } from "src/shared/shared.module"
 import { WorkCenterIDExistenceValidator } from "./custom-validator/workcenterId.validator"
@@ -10,6 +9,7 @@ import { WorkCenterService } from "./workcenter.service"
 
 @Module({
     imports: [
+        /* sadasdadadad */
         WorkCenterDatabaseModule,
         UserDatabaseModule,
         ResourceDatabaseModule,
