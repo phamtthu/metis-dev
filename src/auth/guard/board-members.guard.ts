c3ced8705eb1d18caf91a233253a9ed39bd9c07c
phamhtthu <phamhtthu@gmail.com>
2021-10-15 09:01:42 +0700

    Update Auth guard

--- src/auth/guard/board-members.guard.ts
+++ src/auth/guard/board-members.guard.ts
@@ -19,7 +19,31 @@ export class BoardMemberGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
     try {
       const request = context.switchToHttp().getRequest();
-      const boardId = request.params.boardId;
+      let boardId;
+      if (request.params.boardId) {
+        boardId = request.params.boardId;
+      } else if (request.body.board) {
+        boardId = request.body.board;
+      } else if (request.params.taskId) {
+        const task = await this.authService.findTaskById(request.params.taskId);
+        boardId = task.board;
+      } else if (request.body.task) {
+        const task = await this.authService.findTaskById(request.body.task);
+        boardId = task.board;
+      } else if (request.params.taskChecklistId) {
+        const taskChecklist: any = await this.authService.findTaskChecklistById(
+          request.params.taskChecklistId,
+        );
+        boardId = taskChecklist.task.board;
+      } else if (request.params.taskGroupId) {
+        const taskGroup = await this.authService.findTaskGroupById(
+          request.params.taskGroupId,
+        );
+        boardId = taskGroup.board;
+      } else {
+        throw new Error('Can not find Board');
+      }
+      console.log('BoardID:', boardId);
       const user = request.user;
       const board = await this.authService.checkBoardMember(boardId, user._id);
       if (board) {
