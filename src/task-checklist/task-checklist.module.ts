e1b533080d2dadc079546b55e2c1c96395bf2cba
ThuPham <phamhtthu@gmail.com>
2021-11-03 11:20:56 +0700

    Add ChecklistId validator and fix error

--- src/task-checklist/task-checklist.module.ts
+++ src/task-checklist/task-checklist.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TaskChecklistDatabaseModule } from 'src/model/task-checklist/task-checklist-database.module';
+import { TaskChecklistExistValidator } from './custom-validator/task-checklist-exist.validator';
 import { TaskChecklistController } from './task-checklist.controller';
 import { TaskChecklistService } from './task-checklist.service';
 
 @Module({
   imports: [TaskChecklistDatabaseModule],
   controllers: [TaskChecklistController],
-  providers: [TaskChecklistService],
+  providers: [TaskChecklistService, TaskChecklistExistValidator],
   exports: [TaskChecklistService],
 })
 export class TaskChecklistModule {}
